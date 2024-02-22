d8c68a0c9f5889f43f83f1d2af7a13613978d0fa
Thorkhar <100024582+Thorkhar@users.noreply.github.com>
2024-02-22 15:57:28 +0100

    Config changes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # General settings
-SAMPLE_RATE = '312.5Ms'
+SAMPLE_RATE = '312.5Ms'  # Do not change
+N_POINTS = 65536  # Do not change
 MOKU_A_NAME = 'moku_morgul'
 MOKU_B_NAME = 'moku_tirith'
 MOKU_A_IP = '[fe80:0000:0000:0000:7269:79ff:feb0:0d26%18]'  # Moku Morgul IP
@@ -7,13 +8,12 @@ MOKU_B_IP = ''  # Moku Tirith IP
 
 # Waveform settings
 CHIRP_DURATION = 0.04  # Chirp duration in ms
-N_POINTS = 65536
 F_START = 15000 * CHIRP_DURATION  # Multiply by chirp duration so Moku gets it right
 F_STOP = 3000 * CHIRP_DURATION  # Multiply by chirp duration so Moku gets it right
-REL_RAMP_DURATION = 0.01  # Relative durations of ramp up and down
+REL_RAMP_DURATION = 0.01  # Relative durations of ramp up and down (1 = full wave duration, 0.01 = 1% of total duration)
 PHASES = [
     ch * 45 for ch in range(0, 8)  # Phase per channel in degrees
 ]
 DELAYS = [
-    0, 0, 0, 0, 0, 0, 0, 0  # Delay per channel in microseconds
+    0, 0, 0, 0, 0, 0, 0, 0  # Delay per channel in microseconds (functionality not implemented yet)
 ]
